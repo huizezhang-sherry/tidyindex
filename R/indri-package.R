cbbd441f29e8d2c5deeda641377c54ee9c96a696
Sherry Zhang <huizezhangsh@gmail.com>
2023-01-03 13:38:48 +1100

    update the dataset to include tmax& tmin

--- R/indri-package.R
+++ R/indri-package.R
@@ -1,5 +1,5 @@
 #' @importFrom tidyr pivot_longer unnest
-#' @importFrom dplyr filter mutate group_by  nest_by ungroup quo_name quo enquo pull rowwise tibble arrange
+#' @importFrom dplyr filter mutate group_by nest_by ungroup quo_name quo enquo enquos pull rowwise tibble arrange
 #' @importFrom purrr pmap
 #' @importFrom rlang expr enexpr sym as_label as_function
 #' @importFrom cli cli_abort
--- data-raw/tenterfield.R
+++ data-raw/tenterfield.R
@@ -3,13 +3,16 @@ library(rnoaa)
 library(tidyverse)
 
 tenterfield <- meteo_pull_monitors(
-  monitors = "ASN00056032", var = "PRCP",
+  monitors = "ASN00056032", var = c("PRCP", "TMAX", "TMIN"),
   date_min = glue::glue("1990-01-01"),
   date_max = glue::glue("2020-12-31")
 ) %>%
   mutate(ym = tsibble::yearmonth(date)) %>%
   group_by(id, ym) %>%
-  summarise(prcp = sum(prcp, na.rm =TRUE)) %>%
+  summarise(prcp = sum(prcp, na.rm =TRUE),
+            tmax = mean(tmax/10, na.rm = TRUE),
+            tmin = mean(tmin/10, na.rm = TRUE),
+            tavg = mean((tmax + tmin)/2,  na.rm = TRUE)) %>%
   ungroup()
 
 usethis::use_data(tenterfield, overwrite = TRUE)
