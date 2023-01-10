dd766fee34fe41a7b49df94585e7cb1302f7d4fd
Sherry Zhang <huizezhangsh@gmail.com>
2023-01-10 16:08:32 +1100

    An example on HDI

--- demo/indri.R
+++ demo/indri.R
@@ -27,7 +27,7 @@ library(SPEI)
 (stations <- ghcnd_stations())
 tent_lat <- stations %>% filter(id == "ASN00056032") %>% pull(latitude) %>% unique()
 res2 <- tenterfield %>%
-  init(id = id, time = ym, indicators = prcp) %>%
+  init(id = id, time = ym, indicators = prcp:tavg) %>%
   calc_pet(method = "thornthwaite", Tave = tavg, lat = -29.0479) %>%
   dim_red(expr = prcp - .pet, new_name = "d") %>%
   aggregate(var = d, scale = 12) %>%
@@ -71,5 +71,38 @@ out %>%
 
 
 #######################################################################################
+# Human Development Index
+dt <- readxl::read_xlsx(file.choose(), skip = 4)
+dt <- dt %>%
+  janitor::clean_names() %>%
+  dplyr::select(-paste0("x", seq(4, 14, 2))) %>%
+  dplyr::rename(id = x1, countries = x2) %>%
+  dplyr::mutate(across(c(id, human_development_index_hdi:hdi_rank), as.numeric)) %>%
+  dplyr::filter(!is.na(id)) %>%
+  dplyr::mutate(across(3:7, ~round(.x, digits = 3)))
+colnames(dt) <- c("id", "country", "hdi", "life_exp", "exp_sch", "avg_sch", "gni_pc", "diff", "rank")
 
+# the parameter used here is referenced in the table below at
+# https://hdr.undp.org/sites/default/files/2021-22_HDR/hdr2021-22_technical_notes.pdf
+######################################################
+# Dimension             Indicator                             Minimum     Maximum
+# Health                Life expectancy at birth (years)           20          85
+# Education             Expected years of schooling (years)         0          18
+#                       Mean years of schooling (years)             0          15
+# Standard of living    GNI per capita (2017 PPP$)                100      75,000
 
+# These parameters could be hand-coded, or derived from the data
+# i.e. only a few countries has maximum GNI > 75,000 hence the cap.
+
+# Other indices to compute in the notes (for example MPI - they currently use STATA,
+# but is willing to adopt more R in the future as stated on the website)
+
+dt %>%
+  dplyr::mutate(
+    s_exp_sch = exp_sch/18, # rescaling
+    s_avg_sch = avg_sch/15, # rescaling
+    sch = (s_exp_sch + s_avg_sch) / 2, # dimension reduction
+    life = (life_exp - 20)/(85-20), # rescaling
+    gni = (log10(gni_pc) - log10(100))/(log10(75000) - log10(100)), # rescaling
+    calc = (life * sch * gni)^(1/3) # dimension reduction
+    )
