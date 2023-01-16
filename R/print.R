dcf4d5a40b0cb50e15c1894ed175074c0402024a
Sherry Zhang <huizezhangsh@gmail.com>
2023-01-16 15:06:23 +1100

    print just after init

--- R/print.R
+++ R/print.R
@@ -8,12 +8,15 @@
 print.indri <- function(x, ...){
   cat("Index pipeline: \n")
 
-  cat("\n")
-  cat("Summary: \n")
-
-  op <- x$op %>%
-    rowwise() %>%
-    mutate(print = cli::cli_text("{.emph {module}}: {.code {var}} -> {.field {res}}"))
+  if (nrow(x$op) ==0){
+    cli::cli_text(NULL, default = " Summary: {.field NULL}")
+  } else{
+    cat("\n")
+    cat("Summary: \n")
+    op <- x$op %>%
+      rowwise() %>%
+      mutate(print = cli::cli_text("{.emph {module}}: {.code {var}} -> {.field {res}}"))
+  }
 
   cat("\n")
   cat("Data: \n")
