354a390e8053143e39514655fa9e86eb42611714
Sherry Zhang <huizezhangsh@gmail.com>
2023-01-16 15:07:37 +1100

    the example on calculating HDI using the pipeline

--- R/var-trans.R
+++ R/var-trans.R
@@ -2,17 +2,22 @@
 #'
 #' @param data data
 #' @param method the method use
+#' @param vars variables
 #' @param ... expression
+#' @param param_tbl parameter table
 #' @param new_name the new name
 #'
 #' @return an indri object
 #' @export
-var_trans <- function(data, method = NULL, ..., new_name = NULL){
+var_trans <- function(data, method = NULL, vars,  ..., param_tbl, new_name = NULL){
   dots <- enquos(...)
   method <- enquo(method)
+  vars <- enquo(vars)
   if (inherits(data, "indri")){
     id <- data$roles %>% filter(roles == "id") %>% pull(variables) %>% sym()
-    index <- data$roles %>% filter(roles == "time") %>% pull(variables) %>% sym()
+    if ("time" %in% data$roles$roles){
+      index <- data$roles %>% filter(roles == "time") %>% pull(variables) %>% sym()
+    }
 
     op <- data$op
     roles <- data$roles
@@ -27,15 +32,26 @@ var_trans <- function(data, method = NULL, ..., new_name = NULL){
     fmls <- purrr::map_chr(dots, ~rlang::quo_get_expr(.x) %>% deparse)
     step <- "formula"
   } else{
-    if (is.null(new_name)) new_name <- ".var"
-    new_data <- data %>%
-      dplyr::mutate({{new_name}} := as.vector(do.call(!!method, list(!!!dots))))
+
+
+    if (!rlang::quo_is_null(vars)){
+      vars <- tidyselect::eval_select(vars, data) %>% names()
+      params <- c(list(var = data %>% dplyr::select(vars)), map(dots, ~eval(rlang::quo_get_expr(.x))))
+      new_data <- data
+      new_data[,vars] <- purrr::pmap(params, get(rlang::quo_get_expr(method)))
+    } else{
+      if (is.null(new_name)) new_name <- ".var"
+      new_data <- data %>%
+        dplyr::mutate({{new_name}} := as.vector(do.call(!!method, list(!!!dots))))
+    }
     fmls <- deparse(as.call(c(as.symbol(quo_name(method)), dots)))
     step <- quo_name(method)
+
   }
 
 
   new_var <- setdiff(colnames(new_data), colnames(data))
+  if (length(new_var) == 0) new_var <- "N/A"
   roles <- roles %>%
     dplyr::bind_rows(dplyr::tibble(variables = new_var, roles = "intermediate"))
 
--- demo/indri.R
+++ demo/indri.R
@@ -55,16 +55,21 @@ res3 <- tenterfield %>%
   var_trans(method = thornthwaite, Tave = tavg, lat = -29.0479, new_name = ".pet") %>%
   dim_red(expr = prcp/ .pet, new_name = "r") %>%
   aggregate(var = r, scale = 12) %>%
-  var_trans(y = log10(.agg), .index = (y - mean(y))/sd(y))
+  var_trans(y = log10(.agg),
+            .index = rsc_zscore(y)) # currently rescaling in also implemented under var_trans()
 
-
+# two ways to write it - they are equivalent:
+tenterfield %>%
+  init(id = id, time = ym, indicators = prcp:tavg) %>%
+  var_trans(.var = rsc_zscore(prcp))
+  #var_trans(method = rsc_zscore, var = prcp)
 
 # Effective Drought Index (EDI) - for daily data
-w <- map_dbl(1: 12, ~digamma(.x + 1) - digamma(1)) %>% rev()
+w <- purrr::map_dbl(1: 12, ~digamma(.x + 1) - digamma(1)) %>% rev()
 out <- tenterfield %>%
-  mutate(ep = slider::slide_dbl(prcp, ~sum(.x * w))) %>%
-  mutate(ep_norm = (ep - mean(ep))/ sd(ep))
-
+  init(id = id, time = ym, indicators = prcp:tavg) %>%
+  var_trans(ep = slider::slide_dbl(prcp, ~sum(.x * w)), # this should be a temporal operation
+            ep_norm = rsc_zscore(ep)) # this is rescaling
 
 out %>%
   ggplot(aes(x = ym, y = ep_norm)) +
@@ -80,7 +85,7 @@ dt <- dt %>%
   dplyr::rename(id = x1, countries = x2) %>%
   dplyr::mutate(across(c(id, human_development_index_hdi:hdi_rank), as.numeric)) %>%
   dplyr::filter(!is.na(id)) %>%
-  dplyr::mutate(across(3:7, ~round(.x, digits = 3)))
+  dplyr::mutate(across(4:7, ~round(.x, digits = 3)))
 colnames(dt) <- c("id", "country", "hdi", "life_exp", "exp_sch", "avg_sch", "gni_pc", "diff", "rank")
 
 # the parameter used here is referenced in the table below at
@@ -89,8 +94,8 @@ colnames(dt) <- c("id", "country", "hdi", "life_exp", "exp_sch", "avg_sch", "gni
 # Dimension             Indicator                             Minimum     Maximum
 # Health                Life expectancy at birth (years)           20          85
 # Education             Expected years of schooling (years)         0          18
-#                       Mean years of schooling (years)             0          15
-# Standard of living    GNI per capita (2017 PPP$)                100      75,000
+# Education             Mean years of schooling (years)             0          15
+# Standard of living    GNI per capita (2017 PPP$)                100       75000
 
 # These parameters could be hand-coded, or derived from the data
 # i.e. only a few countries has maximum GNI > 75,000 hence the cap.
@@ -98,7 +103,26 @@ colnames(dt) <- c("id", "country", "hdi", "life_exp", "exp_sch", "avg_sch", "gni
 # Other indices to compute in the notes (for example MPI - they currently use STATA,
 # but is willing to adopt more R in the future as stated on the website)
 
-dt %>%
+
+scaling_params <- tibble::tribble(
+   ~Dimension, ~Indicator, ~Var,  ~Minimum, ~Maximum,
+  "Health",              "Life expectancy at birth (years)",   "life_exp",    "20",          "85",
+  "Education",           "Expected years of schooling (years)",  "exp_sch",    "0",          "18",
+  "Education",           "Mean years of schooling (years)",   "avg_sch",      "0",          "15",
+  "Standard of living",  "GNI per capita (2017 PPP$)",     "gni_pc",       "100",      "75000"
+  ) %>%
+  mutate(across(contains("mum"), as.numeric),
+         across(contains("mum"), ~ifelse(Var == "gni_pc", log10(.x), .x)))
+
+res <- dt %>%
+  init(id = country, indicators = life_exp:gni_pc) %>%
+  var_trans(gni_pc = log10(gni_pc)) %>% # var trans
+  var_trans(method = rsc_minmax, vars = life_exp:gni_pc,
+            min = scaling_params$Minimum, max = scaling_params$Maximum) %>%  # rescaling
+  dim_red(expr = (exp_sch + avg_sch) / 2, new_name = "sch") %>%
+  dim_red(expr = (life_exp * sch * gni_pc)^(1/3), new_name = ".index")
+
+res2 <- dt %>%
   dplyr::mutate(
     s_exp_sch = exp_sch/18, # rescaling
     s_avg_sch = avg_sch/15, # rescaling
@@ -106,4 +130,5 @@ dt %>%
     life = (life_exp - 20)/(85-20), # rescaling
     gni = (log10(gni_pc) - log10(100))/(log10(75000) - log10(100)), # rescaling
     calc = (life * sch * gni)^(1/3) # dimension reduction
-    )
+  )
+
