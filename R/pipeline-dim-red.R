4a4e4772aad1b65864eebeee284e329860807f15
huizezhang-sherry <huizezhangsh@gmail.com>
2023-07-14 14:41:07 +1000

    refactor the dimension reduction module

--- R/pipeline-dim-red.R
+++ R/pipeline-dim-red.R
@@ -1,39 +1,85 @@
-#' Dimension reduction
+#' A dimension reduction module
 #'
-#' @param data data
+#' need detailed documentation on the module
+#' @param data an `idx_tbl` object
 #' @param ... expression to evaluate
-#' @param new_name the new name
 #'
-#' @return a list object
+#' @return an `idx_tbl` object
 #' @export
-dim_red <- function(data, ..., new_name){
-  dots <- enquos(...)
+dimension_reduction <- function(data, ...){
+  dot_name <- names(dots_list(...))
+  dot <- dots_list(...)[[1]]
+  all_attrs <- names(attributes(dot))
 
-  if (!inherits(data, "idx_tbl")) not_idx_tbl()
-
-  id <- data$roles %>% filter(roles == "id") %>% pull(variables) %>% sym()
-  if ("time" %in% data$roles$roles){
-    index <- data$roles %>% filter(roles == "time") %>% pull(variables) %>% sym()
+  if ("var" %in% all_attrs){
+    v <- attr(dot, "var")
+    vars <- tidyselect::eval_select(parse_expr(v), data$data)
+    vars_nm <- names(vars)
   }
-  op <- data$op
-  roles <- data$roles
-  data <- data$data
 
-  # currently only formula
-  new_data <- data %>% dplyr::mutate(!!!dots)
-  fmls <- purrr::map_chr(dots, ~rlang::quo_get_expr(.x) %>% deparse)
-  step <- "formula"
-  new_var <- names(dots)
+  if ("weight" %in% all_attrs){
+    w <- attr(dot, "weight")
+    weight <- tidyselect::eval_select(parse_expr(w), data$roles)
+    weight_nm <- names(weight)
+  }
 
-  roles <- roles %>%
-    dplyr::bind_rows(dplyr::tibble(variables = new_var, roles = "intermediate"))
-  op <- op %>%
-    dplyr::bind_rows(dplyr::tibble(
-      module = "dim_red", step = "formula", var = fmls, res = new_var
-    ))
+  if ("formula" %in% all_attrs){
+    dot_fml <- attr(dot, "formula") %>% parse_expr()
+  }
 
-  res <- list(data = new_data, roles = roles, op = op)
-  class(res) <- c("idx_tbl", class(res))
-  return(res)
+  if (dot == "aggregate_linear"){
+    weight <- data$roles %>% filter(variables %in% vars_nm) %>% pull(weight_nm)
+    pieces <-  paste0(vars_nm, "*", weight, collapse = "+")
+    dot_fml <- paste("~", pieces) %>% as.formula() %>% f_rhs()
+    data$data <- data$data %>% mutate(!!dot_name := eval_tidy(dot_fml, data = .))
+    dot_fml <- pieces %>% sym()
+  }
 
+  if (dot == "aggregate_geometrical"){
+    dot_fml <- glue::glue("~(", paste0(vars_nm,  collapse = "*"), ")^(1/{length(vars_nm)})") %>% as.formula() %>% f_rhs()
+    data$data <- data$data %>% mutate(!!dot_name := eval_tidy(dot_fml, data = .))
+  }
+
+  if (dot ==  "manual_input"){
+    data$data <- data$data %>% mutate(!!dot_name := eval_tidy(dot_fml, data = .))
+  }
+
+  data$roles <- data$roles %>%
+    dplyr::bind_rows(dplyr::tibble(variables = dot_name))
+  data$op <- data$op %>% dplyr::bind_rows(dplyr::tibble(
+    module = "dimension_reduction",
+    step = as.character(dot),
+    var = deparse(dot_fml),
+    res = dot_name))
+
+  return(data)
+}
+
+#' @export
+aggregate_linear <- function(formula, weight){
+  vars <- rlang::f_text(formula)
+  weight <- enquo(weight) %>% quo_text()
+  new_dimension_reduction("aggregate_linear", vars = vars,  weight = weight, formula = NULL)
+}
+
+#' @export
+aggregate_geometrical <- function(formula, weight){
+  vars <- rlang::f_text(formula)
+  new_dimension_reduction("aggregate_geometrical", vars = vars,  weight = NULL, formula = NULL)
+}
+
+#' @export
+manual_input <- function(formula){
+  formula <- rlang::f_text(formula)
+  new_dimension_reduction("manual_input", formula = formula, vars = NULL, weight = NULL)
+}
+
+#' @export
+new_dimension_reduction <- function(type, formula, vars, weight){
+  name <- type
+  attr(name, "var") <- vars
+  attr(name, "weight") <- weight
+  attr(name, "formula") <- formula
+  class(name) <- "dim_red"
+  name
 }
