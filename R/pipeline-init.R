0a4a24f59b39f2934fcba29a6e1476c4e9c0f209
huizezhang-sherry <huizezhangsh@gmail.com>
2023-07-14 14:40:43 +1000

    add functionality to modify metadata (roles table)

--- R/pipeline-init.R
+++ R/pipeline-init.R
@@ -6,20 +6,15 @@
 #' @return a list object
 #' @export
 init <- function(data, ...){
-
   dots <-  dplyr::enquos(...)
+
+  # role of a variable: indicators, pillar, index, others
+  # TODO rename: roles -> meta, ops -> steps
   roles <- tibble(
     variables = map(dots, ~tidyselect::eval_select(.x, data) %>% names()),
     roles = names(dots)) %>%
     unnest(variables)
-
-  no_roles <- !names(data) %in% roles$variables
-  if (any(no_roles)){
-    no_roles_tibble <- dplyr::tibble(variables = names(data)[no_roles]) %>%
-      mutate(roles = NA)
-    roles <- roles %>%
-      rbind(no_roles_tibble)
-  }
+  roles <- dplyr::tibble(variables = colnames(data)) %>% left_join(roles)
 
   op <- dplyr::tibble(op = NULL)
 
@@ -28,5 +23,30 @@ init <- function(data, ...){
   return(res)
 }
 
+#' @export
+add_meta <- function(data, new_meta, var_col){
+  var_col <- enquo(var_col) %>% rlang::quo_name()
+  if (!inherits(data, "idx_tbl")) not_idx_tbl()
 
+  lhs_by <- colnames(data$roles)[1]
+  data$roles <- data$roles %>% left_join(new_meta, by = setNames(var_col, lhs_by))
+  return(data)
+}
+
+#' @export
+update_meta_cell <- function(data, variable, col, value){
+  variable <- enquo(variable) %>% rlang::quo_name()
+  col <- enquo(col) %>% rlang::quo_get_expr()
+  if (!inherits(data, "idx_tbl")) not_idx_tbl()
+  data$roles <- data$roles %>% mutate(!!col := ifelse(variables == {variable}, value, !!col))
+  cli::cli_inform("metadata {.var {col}} for {.var {variable}} has been changed to {value}")
+  return(data)
+}
+
+#' @export
+get_meta <- function(data, vars, cols){
+  # check data is the role table of the idx_tbl
+  cols <- enquo(cols) %>% rlang::quo_get_expr()
+  data %>% filter(variables %in% vars) %>% pull({cols})
+}
 
